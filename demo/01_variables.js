3c21105cbca46f42170f1a28f235462a89299a89
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2023-10-07 22:01:24 +0000

    Learned Variables from vairiable.js

---
+++ demo/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 123423423;
+let accountEmail = 'vaibhav123@gmail.com';
+var accountPassword = '401501';
+accountCity = 'Gurugram';
+let accountState;
+// accountId = 123;
+
+
+// console.log(accountId);
+
+accountEmail = 'timepass@timepass.com';
+accountPassword = 2323232;
+accountCity = 'Boisar';
+
+console.table([accountEmail, accountId, accountId, accountPassword, value ='demo', accountState]); 
+/* Output for the Demo
+┌─────────┬─────────────────────────┐
+│ (index) │         Values          │
+├─────────┼─────────────────────────┤
+│    0    │ 'timepass@timepass.com' │
+│    1    │        123423423        │
+│    2    │        123423423        │
+│    3    │         2323232         │
+│    4    │         'demo'          │
+│    5    │        undefined        │
+
+*/
