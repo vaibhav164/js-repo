a80655bc78616dcfbc36eba846cafea7bfd0ffa5
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2023-10-07 22:51:09 +0000

    discussed Data type

---
+++ demo/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"  //this is commant used to tell JS engine to execute code in strict mode
+
+// alert(3 + 3) this will not work as we are using node js not bowser as it has document binded with it to execute alert
+
+console.log(2
+    +
+    3) //code readablity shouid be high 
+
+
+    let name1 = 'vaibhav';
+    let age = 27;
+    let city = 'Nagpur';
+
+
+    //number => has a range of 2 to the power of 52 or 53;
+    // bigint =>it is used for bigger number then the above value
+    // string =>"Hellow world"
+    // boolean => true/false
+    // null => is a stand alone value it is a representation of no value is present i.e., empty kind of 
+    // undefined => is it value or datatype of a variable which is declared but has a assigned value e.g., let name;
+    // symbol => it is a type when we have to make a variable unique
+    
+
+    //object
+
+    console.log(name1);
+    console.log(typeof null);  // output for this is object!!!!!!!
+    console.log(typeof undefined); // output for this is undefined....
+ 
