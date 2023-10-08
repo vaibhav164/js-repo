d10cd747c1b41ce7c9ce679a4bac70bb39b5b9ec
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2023-10-08 01:22:01 +0000

    learned comparisions

---
+++ demo/05_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2>=1)
+
+// console.log("3">1);
+// console.log("3"<3);
+
+
+// console.log(null > 0);//=>false
+// console.log(null == 0);//=>false
+// console.log(null < 0);//=>false
+// console.log(null >= 0);//=>true
+/*
+as we can see in line no 10 and line no 11 that null is not greater than 0 and not is equeals to 0 but in JS conversion algo what we 
+compare it using >= operator it consider's null as greater than or eqauls to 0 thus all ther statements are false but last one is true !!!!!!!!!!!
+*/
+
+
+
+console.log(undefined > 0); //=>false
+console.log(undefined >= 0); //=>false
+console.log(undefined == 0); //=> false
+
+/* **********Important note here is this that when we do comparisions we shol fir make the type of the variable same */
+
