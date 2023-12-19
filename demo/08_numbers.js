906c37f03b8e9609b06b53377ad99c52bd94807e
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2023-12-19 18:55:44 +0000

    Learned Number.MAX_SAFE_INTEGER and Number.MIN_SAFE_INTEGER

--- demo/08_numbers.js
+++ demo/08_numbers.js
@@ -3,10 +3,10 @@ const marks = 400
 // new way to define a number is as below
 
 const score = new Number(30.345345345);
-// console.log(marks);
-// console.log(score);
+console.log(marks);
+console.log(score);
 
-// console.log(score.toString());
+console.log(score.toString());
 
 //tofixed method
 // this will let us get the value of number precised till the decimal point passed in method
@@ -17,7 +17,7 @@ const score = new Number(30.345345345);
 //toPrecision method
 //this will work for the condtion to make a number precised
 // this is used to get a precised value upto a point which is passed and it returns a string
-// console.log(score.toPrecision(3));
+console.log(score.toPrecision(3));
 
 
 //toLocaleString method
@@ -30,5 +30,9 @@ console.log(demoNumber.toLocaleString('en-IN'));
 // if number is demoNumber Value then if console.log(demoNumber.toLocaleString())) => it O/p will be as 3,453,453,454,645
 // and if same is of this log console.log(demoNumber.toLocaleString('en-IN')); tis value will became as per indian standard 34,53,45,34,54,645
 
+// Number.MAX_SAFE_INTEGER and Number.MIN_SAFE_INTEGER
+//this will give max or min number that can be used in JS using number
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
 
 
