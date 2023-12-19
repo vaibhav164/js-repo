bc778284d70950326364123ade80416000b9b22c
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2023-12-19 06:54:14 +0000

    working in string

---
+++ demo/07_strings.js
@@ -0,0 +1,10 @@
+//new way to delare a string 
+
+const newStringDeclare = new String('Hellow bro');
+const newString= "New String";
+
+console.log(newString[2]);
+console.log(newStringDeclare);
+console.log(newString.__proto__);
+console.log(newStringDeclare.toUpperCase());
+console.log(newStringDeclare.indexOf('o'));
