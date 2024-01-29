ea6edfa50bb47fc9383bf808db8ff732ae2e472a
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-01-29 07:26:23 +0000

    looked on for in loop in js

--- demo/09_forOfLoops.js
+++ demo/09_forOfLoops.js
@@ -5,3 +5,9 @@ for(name1 of names){
 }
 
 //here name1 is a value of specific index of names array as per its instance
+
+for(name2 in names){
+        console.log(name2)
+}
+
+//here in for in loop the name2 will give the index of each element and this loop will iterate till the length of array
