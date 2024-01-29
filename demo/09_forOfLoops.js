cef9f40f5b54f32d7bdaf4c7bcc27542edca9d8f
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-01-29 06:36:34 +0000

    learned about for of loop in JS

---
+++ demo/09_forOfLoops.js
@@ -0,0 +1,7 @@
+const names=['ajay','arman', 'vishal', 'vivek','tarun'];
+
+for(name1 of names){
+        console.log(name1);
+}
+
+//here name1 is a value of specific index of names array as per its instance
