d49fa9310bd7996961329060282878072648bd9e
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-01-29 11:09:26 +0000

    Worked on Objects

--- demo/12_objects.js
+++ demo/12_objects.js
@@ -7,4 +7,20 @@ const person ={name:'vaibhav',age:27,location:'grugaon',nationality:'Indian'};
 
 person.contactNumber = 9158402302;
 
-console.log(person);
+//console.log(person);
+//how to access the value of object without using dot '.' notation
+
+//so the key name in objets are by default of string type so in about the key name or any other key as age, location, nataionality are of string type
+
+//to access any value in object we ca do
+
+//console.log(person['name']);
+
+const people ={'name':'userName','roleNo':1232,'age':34}
+
+// console.log("pople______",people['roleNo']);
+
+//as we can access the value using bracket notation same we can add a valve in people object
+
+people['contactNumber']=9158402302;
+console.log(people)
