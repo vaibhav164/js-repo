d8f3a118f2e5ee44e21436c51fc7e80c45640446
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-01 18:17:35 +0000

    revised array destructuring

--- demo/11_arrayDesctructuring.js
+++ demo/11_arrayDesctructuring.js
@@ -3,11 +3,22 @@ const fruits= ['mango','grapes','guvava','berrys','apple', 'banana'];
 
 const [value1, value2, ...remaingFruits] = fruits;
 
-console.log("value fo value1",value1);
-console.log('value of value2',value2);
-console.log("remaingFruits are these",remaingFruits);
+// console.log("value fo value1",value1);
+// console.log('value of value2',value2);
+// console.log("remaingFruits are these",remaingFruits);
 
 const [val1,val2] = remaingFruits;
 
-console.log('value fo val1',val1);
-console.log('value fo val2',val2);
+// console.log('value fo val1',val1);
+// console.log('value fo val2',val2);
+
+
+const destructureArray = ['One',"two", "three", 'four', 'five'];
+
+const [first1, second2, val3, ...restValueArray]=destructureArray;
+
+console.log('new demo for array destructurring of array');
+console.log(first1);
+console.log(second2);
+console.log(val3);
+console.log(restValueArray);
