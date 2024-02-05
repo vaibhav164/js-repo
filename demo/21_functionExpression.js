9fa5fec16896c53bb4e862134592ef1eb43fa37a
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-05 19:36:42 +0000

    Learned function expression

---
+++ demo/21_functionExpression.js
@@ -0,0 +1,18 @@
+/**
+ normaly we declare functions as below
+ */
+
+ function logSong(){
+    console.log('Hello bro is a song we are singing')
+ }
+
+ //now lets see how to make an expression function
+
+
+ const singASong= function (){
+    console.log('Jungle jungle par chali hai pata chala hai chadhi pehen ki phool khila hai phoole khila hai')
+ }
+
+ singASong();
+
+ //so above L12 is function expression
