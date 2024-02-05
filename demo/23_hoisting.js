da3507b59b621e38d8e2eb589fab924c7af6e262
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-05 20:18:29 +0000

    First discovered diffrence between arrow and normal function is of hoisting

---
+++ demo/23_hoisting.js
@@ -0,0 +1,26 @@
+//hoisting
+
+normalfunction();
+
+function normalfunction(){
+    console.log('normal function log');
+}
+
+/**
+ here in above function is a normal function it can be called before declaration
+ *
+ */
+arraowFunction();
+
+
+/**
+ * 
+ but the arrow function throws error as arraowFunction cannot be used before 
+ declaration this is known as hoisting
+ * 
+ * 
+ */
+
+const arraowFunction=()=>{
+    console.log('log for arrow function =>*************')
+}
