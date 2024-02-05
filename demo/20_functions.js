31178e147b8f3149444f17e7d8e5693e1c794fae
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-05 19:01:18 +0000

    Refreshing memoryof functions parameter and argument

---
+++ demo/20_functions.js
@@ -0,0 +1,17 @@
+// value passed in fuctions is called are argument 
+//e.g., in L11 the value 5 and 7 are arguments of sumTwoNumber function
+
+/*value which is recived in function while declaring a function is called are 
+  parameters so in L7 num1 and num2 are paramter of sumTwoNumber
+*/
+function sumTwoNumber(num1, num2){
+    return num1 + num2;
+}
+
+sumTwoNumber(5,7);
+
+/**
+ undefiend + undefined = NaN
+ or any number summed with undefined is NaN 
+ e.g., 3+4+undefined=NaN
+ */
