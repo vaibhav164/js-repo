905fb6e4d73ab997edc8f9f950addfe3dabf2a08
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-05 18:42:00 +0000

    Learing Nested destructuring

---
+++ demo/19_ObjectsInArray.js
@@ -0,0 +1,22 @@
+/** 
+ Objects in Array
+*/
+
+const foodList=[
+    {name:'kheer', taste:'sweet'},
+    {name:'aludum', taste:'green spicy'},
+    {name:'neemona', taste:'salty'},
+    {name:'puri', taste:'crispy'},
+    {name:'gajar ka Halwa', taste:'sweet'}
+];
+
+const [first, second, third, ...restData]=foodList;
+
+const [{taste:firstDishTaste},,,,{name:fifthDishName}]=foodList;
+/**
+we can update for this value
+first.name = null;
+console.log(foodList);
+ */
+
+ console.log('first', firstDishTaste, 'second', fifthDishName);
