7a9d4b69f4cb3a83b4704c32e14e9c252e77f81a
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-22 11:14:14 +0000

    Learned the function for forEach

---
+++ demo/31_array_methods.js
@@ -0,0 +1,24 @@
+//foreach
+//reduce
+//map
+//filter
+
+let demoArray=[3,5,4,3,6,5,8,7];
+/* filter and map mehtods return type is always an array*/
+let filterArray=demoArray.filter(item=>{
+    if(item > 4){
+        return item;
+    }
+})
+
+// console.log('demoArray_____',demoArray.length ,"\n demoArray",demoArray)
+// console.log('filterArraylength_____',filterArray.length ,"\n filterArray",filterArray)
+
+/*********** For Eachhhhhhhhh loop for array
+ forEach method of array takes an callback fucntion as parameter and  that call back fuction 
+ may can have 2 paramenters one as value of index and other will be index of the value
+ */
+function multtiplyBy2(value, index){
+    console.log(`value is ${value} and index is ${index}`)
+}
+demoArray.forEach(multtiplyBy2)
