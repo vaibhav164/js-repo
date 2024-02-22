8b6bc0d7a2ff0b4bdc4cc0dea9aad85633e56e07
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-22 20:28:17 +0000

    Learned that in reduce function in array we have to return accumulator in each itteration else we will not get required response from it

--- demo/32_reduce_array.js
+++ demo/32_reduce_array.js
@@ -3,7 +3,10 @@
 const trailArray = [4,5,3,5,6,6,7,8];
 
 /**
- reduce fuction in array takes an callback function as parameter and that fuction
+ reduce fuction in array takes an callback function and an optional parameter
+ which is the innitial value of accumulator i.e., if we pass the initial value
+ to accumulator the current value will cover from first value
+ as parameter and that fuction 
 takes 2 values 1st is an accumulator and other one is sum we can call them as any 
 thing but first value will refer to accumumlater and other as current value 
  */
@@ -14,9 +17,13 @@ const sum = trailArray.reduce((accumulator, currentValue)=>{
     return accumulator + currentValue;
 })
 
-console.log(sum)
-
-//so L17 will give a value of sum of above mentioned array
+const initialSumDemo = trailArray.reduce((accumulator, curentvalue)=>{
+    return accumulator + curentvalue;
+},20)
+// console.log(sum)
+// so in L20 to L22 it will have initial value of 20 to accumulator and the first
+//value for curentValue will be 4 on trailArray
+//so L23 will give a value of sum of above mentioned array
 
 // [4,5,3,5,6,6,7,8]
 //    accumulator  currentValue return
@@ -30,4 +37,24 @@ console.log(sum)
 
 
 
-anuman
+let testArray =[3,3,4,2,5,3,34];
+const result = testArray.reduce((accumulator,curentValue)=>{
+    if(curentValue%2 == 0){
+    return accumulator + curentValue;
+    }
+    return accumulator;
+},0)
+
+console.log(result)
+
+const productCart =[
+    {productName:'phone', productId:1,productPrice:2000},
+    {productName:'pen', productId:2,productPrice:20},
+    {productName:'bag', productId:3,productPrice:200},
+];
+
+const billAmount = productCart.reduce((totalPrice,curentProduct)=>{
+    return totalPrice + curentProduct.productPrice;
+},0)
+
+// console.log(billAmount)
