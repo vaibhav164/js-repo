c36832d6e6aa0d78c3d52aa5461919b225efbdc4
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-23 05:42:40 +0000

    learning aboutiterable and array llike objects

---
+++ demo/35_itterable_arrayLikeObjects.js
@@ -0,0 +1,15 @@
+/** iterable are thoes elements in JS to which we can use loops 
+ * e.g., string or array */
+
+/*** array like objects as we know objects are not iterable but array like objects are
+ * those who hase length property and can be accessed using index e.g., string;
+ */
+
+let names = 'vaibhav';
+
+// console.log(names.length);
+// console.log(names[4], names['4'])
+
+/*** as we can see in L10 and L11 we can access the length of names string and 
+ * can access the string using index thus it can be called as array like objects
+ */
