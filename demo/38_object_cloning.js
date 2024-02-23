27aa53cd0eef96c360e414058ec4f1d49b630a62
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-23 07:34:52 +0000

    learned to cloan object using Object.assign in JS

---
+++ demo/38_object_cloning.js
@@ -0,0 +1,23 @@
+const newObject ={
+    name:'demo name',
+    age:33
+};
+
+// const secondObject = {...newObject};
+
+newObject.key3 = 'test';
+console.log(newObject)
+// console.log(secondObject)
+
+/***** above we are cloning object sing spred operator but can also be done using 
+ * assign method
+ * */
+
+const thirdObj = Object.assign({},newObject);
+
+console.log(thirdObj)
+
+//so L16 creates an Object as its inserting an newBoject keys value pairs to empty
+//object
+//Object.assign takes two pramwenters one is destination object and send as object
+// to be merged 
