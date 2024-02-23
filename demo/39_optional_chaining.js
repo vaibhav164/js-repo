1f6e39350084d11153adda5e8e2113239bd2f87a
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-23 20:49:47 +0000

    Learned about this keyword in Object in JS

--- demo/39_optional_chaining.js
+++ demo/39_optional_chaining.js
@@ -8,12 +8,61 @@ const userDetail ={
     name:'vaibhav',
     age:23,
     call: function(){
-        console.log(`Person's name is ${this.name} and age is ${this.age}`)
+        console.log(this)
     }
 }
 
-userDetail.call();
+// userDetail.call();
 userDetail.name = 'vishal';
-userDetail.call();
+// userDetail.call();
 
 //so to access data of specific object instance we use this keyword
+// this in any object represent the current object 
+//as in L11 log of this give the value of complete objects as below
+/***
+ { name: 'vaibhav', age: 23, call: [Function: call] }
+ */
+function printPernInfo(){
+    console.log(`Person name is ${this.name} and age is ${this.age} lives in ${this.state}`)
+}
+
+ const person1 ={
+    name:'vaibhav',
+    age:23,
+    state:'maharashtra',
+    action:printPernInfo
+ };
+ const person2 ={
+    name:'Vishal',
+    age:23,
+    state:'UP',
+    action:printPernInfo
+ };
+ const person3 ={
+    name:'Ashish',
+    age:23,
+    state:'Odisa',
+    action:printPernInfo
+ };
+
+//  person1.action();
+//  person2.action();
+//  person3.action();
+
+ //thus we can conclude that this works only for normal function declaration 
+ /*** not in arrow function and this in normal function delcaration represents 
+  * instance of object in short to that object
+  *  */  
+
+ //this gives value 
+
+
+ const thisTest ={
+    name:'vaibhav',
+    age:23,
+    action:function valueCall(){
+        console.log(this)
+    }
+ }
+
+ thisTest.action();
