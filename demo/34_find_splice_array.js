1a6972636707e1250fddb263118a9eb40fc9fb24
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-02-23 03:51:17 +0000

    Learned about every method in array in JS

--- demo/34_find_splice_array.js
+++ demo/34_find_splice_array.js
@@ -23,7 +23,35 @@ const userList =[
 
 const userWithUserId6 = userList.find((user)=>user.userId === 6);
 
-console.log(userWithUserId6);
+// console.log(userWithUserId6);
 
 //so the find function is used in array method to find a value in array which is 
 //unique
+
+/* EVERY METHOD */
+const numbers = [4,6,8,0,2,44];
+
+const is_array_even = numbers.every((num)=>num%2 === 0);
+
+// console.log(is_array_even)
+
+
+const applicantList =[
+    {name:'vaibhav', age:19, gender:'M'},
+    {name:'vai', age:43, gender:'M'},
+    {name:'vav', age:45, gender:'M'},
+    {name:'bhav', age:20, gender:'M'},
+    {name:'ved', age:19, gender:'M'},
+    {name:'vinod', age:40, gender:'M'},
+    {name:'vinay', age:22, gender:'M'},
+    {name:'vipin', age:25, gender:'M'},
+];
+
+const are_applicants_adult = applicantList.every((value)=>value.age>=18) 
+
+console.log(are_applicants_adult)
+
+
+//so above is an example of every method of array it takes an call back function
+//return only tru of all condition in callback for each element in array is true
+//else false
