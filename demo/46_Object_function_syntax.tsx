315ae40caa17afd75ebf1cd76cbb9bc5a2e06b17
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-04-08 19:50:31 +0000

    Learned about a new syntax of decalraying an method and use it in Objects in Js

---
+++ demo/46_Object_function_syntax.tsx
@@ -0,0 +1,26 @@
+/*** Object function short syntax__________ */
+/****
+const objectDemo ={
+    name:'Hello',
+    age:34,
+    demofunction:function(){
+        console.log(`${this.name}`)
+    }
+}
+ */
+
+/***
+ thus an fucntion in object may can have a short syntax as below
+ */
+
+ const objectDemo ={
+    name:'Hellow',
+    age:34,
+    demofunction (){
+        console.log(`${this.name}`)
+    } 
+ }
+
+ /***
+  so L6 and L19 is same we can use both way to declare and use function
+  */
