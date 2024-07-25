0bd0ab06431822ec5829ce716b806759aedd6cbf
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-07-25 03:12:54 +0000

    Learned about prototype of function and its scope in arrow and normal function

--- demo/52_Object.js
+++ demo/52_Object.js
@@ -1,12 +1,23 @@
-//Linking of Object key with other object key
+//In JS function can also be treated as object
 
-const obj1 = {
-    key1:'Value1',
-    key2:'Value2'
+ function hey() {
+    console.log("Inside hey")
+}
+// hey.call();
+// console.log(hey.name) 
+
+hey.newProperty = 'demo bro of key property of function'
+
+// console.log(hey.newProperty)
+
+//here you can see that function can act as object and here is the above example
+hey.prototype.value = function(){
+    console.log("inside value prototype in hey function")
+}
+hey.prototype.terms = function(){
+    console.log("inside trerms of prototype of hey")
 }
 
-const obj2 = Object.create(obj1)
+hey.prototype.term2="prototype term2 key value"
 
-console.log(obj2.__proto__)
-
-//__proto__ is aslo called as dunder
+console.log(hey.prototype.term2)
