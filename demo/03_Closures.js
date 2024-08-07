21f6e17232c0a5848ae475bd1e37ba2ded7ea0d9
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-08-07 15:34:44 +0000

    Learned about closure and lexical scope and some other way of returning a funciton

--- demo/03_Closures.js
+++ demo/03_Closures.js
@@ -9,9 +9,28 @@ function outerSum(val1){
 
 const result = outerSum(5);
 
-console.log(result) //[Function: innerSum]
+//console.log(result) //[Function: innerSum]
 
 //you can notice that above log will log the return fucntion if you want to log returned value then you have to call it and as 
 //while calling the return function it take one additional parameter thus you need to pass an other parameter as below
 
-console.log(result(3));//O/P--->>>>> 8
+//console.log(result(3));//O/P--->>>>> 8
+
+
+function loadbalance (){
+    let currentBalance = 20;
+
+
+    function UpdateBalance(){
+        let newValue = ++currentBalance
+        return newValue
+    }
+
+    return {UpdateBalance};
+};
+
+const balanceValue = loadbalance();
+
+//console.log(balanceValue())//this will throw error because it loadBalance will return an object which has a function;
+console.log(balanceValue.UpdateBalance())
+console.log(Object.values(balanceValue))// O/P--->>[ [Function: UpdateBalance] ] JFYI
