c81150d62ab45e4f5ec2870e142f1edecfb0864d
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-12-05 15:11:52 +0530

    Update 63_ajax.js
    Learning about AJAX and xmlHttpRequest

--- demo/63_ajax.js
+++ demo/63_ajax.js
@@ -1,1 +1,14 @@
+/**
+AJAX : asyncronous Javascript and XML
 
+HTTP request is a set of web application texhniques using many web technologies on the client side to create asynchronous web app
+
+with AJAX web apps can send and retrive data from server asynchronously(in the background) without interfering with display and behaviour of the
+existing page
+
+
+We have 3 most common ways to create and send request to server
+1. xmlHttpRequest (old way of doing)
+2. fetch (new way of doing introduced in ES6)
+3. axios (3rd party library)
+*/
