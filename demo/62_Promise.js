8961d557fd4cf60a9fcc15638eae6c3ac35912db
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-12-05 11:24:33 +0530

    Update 62_Promise.js
    Ahdaar card example for Promise

--- demo/62_Promise.js
+++ demo/62_Promise.js
@@ -1,1 +1,22 @@
+let data = ['photo','phonenumber', 'dob', 'fingerprint'];
 
+const adhaarCard = new Promise((res, rej)=>{
+    if(data.includes('photo') && data.includes('dob') && data.includes('phonenumber') && data.includes('fingerprint')){
+        const Data = {
+            'name':"Vaibhav Singh",
+            'age':45,
+            'gender':'Male',
+            'adhaar_number':'2845 7011 4040',
+            'message':'Congrations!!!!! your Adhaar in Progress'
+        }
+        res(Data)
+    }else{
+        rej("Try Again!!!!!")
+    }
+});
+
+adhaarCard.then((data)=>{
+    console.log(`${data.name} ${data.message}`)
+}).catch((data)=>{
+    console.log(data)
+})
