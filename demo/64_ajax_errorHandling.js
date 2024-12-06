0e22e4560b4073094172691a884bb0c56b77968f
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2024-12-06 14:29:35 +0530

    Update 64_ajax_errorHandling.js
    Updating Handling of error in XMLHttpRequest api call logic

--- demo/64_ajax_errorHandling.js
+++ demo/64_ajax_errorHandling.js
@@ -1,1 +1,21 @@
+// Let's see how to handle AJAX error handling for XMLHttpsRequest
 
+const URL = 'https://jsonplaceholder.typicode.com/postsSSSSSSS'
+const apiCall = new XMLHttpRequest();
+
+apiCall.open("GET", URL);
+
+apiCall.onLoad = ()=>{
+  if(apiCall.status >=200 && apiCall.status < 300){
+    const data = JSON.parse(apiCall.response);
+    console.log(data)
+  }else {
+  console.log("Something went wrong")
+}
+}
+
+apiCall.onerror = () =>{
+  
+}
+
+apiCall.send();
