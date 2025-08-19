b06dbb53ae8e75436fe86b9d61781cf95877485d
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2025-08-19 20:13:28 +0000

    Learned never, void and return type of function in JS

--- TypeScript/Function.ts
+++ TypeScript/Function.ts
@@ -14,3 +14,21 @@ const ansu =():number=>{
     return six
 }
 //In Above fuction if we'll return anything except a number the TS will throw error
+
+/**** Function Returing void or not returing anything */
+
+const throwError =(errorMessage:string):void=>{
+    console.log(errorMessage)
+    //Now if i return anything TS will throw error
+}
+
+const handleError =(err:string):never=>{
+    throw new Error(err)
+    /***now if i return somthing in this function or do something except throwing error TS will throw below error
+     * 
+     * -----A function returning 'never' cannot have a reachable end point.------
+     * 
+     * It means that function throws an exception or terminates the program
+     * it appears where there is nothing left in union 
+     * **/
+}
