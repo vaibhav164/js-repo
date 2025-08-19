caffbadb7b982184cfa4d8d336f78e21977ebb3a
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2025-08-19 19:54:57 +0000

    Learning about function in TS

---
+++ TypeScript/Function.ts
@@ -0,0 +1,11 @@
+/****
+ * 
+ * By this below way we can define that what will be the type of return of the function
+ */
+
+function vaibhav():number{
+    const five = 5;
+
+    return five
+}
+//In Above fuction if we'll return anything except a number the TS will throw error
