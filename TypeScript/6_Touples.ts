cc7fc1cd410c9e4b4981d99636cf3f7c09e7357f
Vaibhav Singh <68284839+vaibhav164@users.noreply.github.com>
2025-08-20 18:52:58 +0000

    Learned about Tuples and its uses

---
+++ TypeScript/6_Touples.ts
@@ -0,0 +1,16 @@
+//Touples is used in TS to define the squese of type of element in array in JS below is the example
+
+
+let data :[number, string, boolean, string]
+
+//Now TS will throw error if i shuffle the order of elements in data e.g.,
+
+data = [3443,'dsfsf',false,'test'];
+
+data = [344343, true, 'dsscc','ddtdg'];
+//As you can see at L10 its giving error as its not matching to its touple
+
+/**** Issue in touples in TS is that as below  as we can push any value to the data array even if we have defined its touples*/;
+data.push("Test")
+data.push(false)
+data.push(43433434)
